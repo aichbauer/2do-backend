cdab6de34e9d62dc97f28fe48b2e394b8b833706
Student <student@fhstp.ac.at>
2016-06-02 21:02:43 +0200

    init commit

---
+++ api/controllers/TodoController.js
@@ -0,0 +1,62 @@
+/**
+ * TodoController
+ *
+ * @description :: Server-side logic for managing todoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  createTodo: function (req, res) {
+
+    var body = req.body;
+
+    Todo.create({
+      task: body.task,
+      isDone: false
+    }).then(function (todo) {
+      console.log(todo);
+      return res.ok();
+    }).fail(function (err) {
+      console.log(err);
+      return res.negotiate(err);
+    });
+  },
+  getTodos: function (req, res) {
+    Todo.find().then(function (todos) {
+      return res.json(200, {todos: todos});
+    }).fail(function (err) {
+      console.log(err);
+      return res.negotiate(err);
+    });
+  },
+  updateTodo: function (req, res) {
+
+    var id = req.param('todoid');
+
+    Todo.update({id: id}, req.body)
+      .then(function (updatedTodo) {
+        if (!updatedTodo[0]) return res.json(404, {err: 'todo not found!'});
+        return res.json(200, {todo: updatedTodo});
+      })
+      .fail(function (err) {
+        return res.negotiate(err);
+      })
+  },
+
+  deleteTodo: function(req, res){
+    var id = req.param('id');
+
+    Todo.destroy({
+        id: id
+      })
+      .then(function(){
+        return res.ok();
+      })
+      .fail(function(err){
+        return res.negotiate(err);
+      })
+
+  }
+};
+
---
+++ api/models/Todo.js
@@ -0,0 +1,24 @@
+/**
+ * Todo.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  schema: true,
+
+  attributes: {
+    task: {
+      type: 'string',
+      required: true
+    },
+    isDone: {
+      type: 'boolean',
+      defaultsTo: false,
+      required: true
+    }
+  }
+};
+
